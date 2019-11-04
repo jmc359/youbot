932bb2fa7f0db61af4bab4aa4ab9d85f0431795e
jmc359 <jconnolly171@gmail.com>
2019-11-04 11:05:34 -0500

    added world

---
+++ controllers/youbot/build/release/youbot.d
@@ -0,0 +1,15 @@
+build/release/youbot.o: youbot.c \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/supervisor.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/projects/robots/kuka/youbot/libraries/youbot_control/include/arm.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/projects/robots/kuka/youbot/libraries/youbot_control/include/base.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/projects/robots/kuka/youbot/libraries/youbot_control/include/gripper.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/compass.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
+ youbot_zombie_1.h
