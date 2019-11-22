2a1c0493ef92a7220240c5418ddcd189d36d4b2f
Sydney Thompson <sydney.thompson@yale.edu>
2019-11-22 10:24:07 -0500

    Removing unused helper functions

--- controllers/youbot/youbot.c
+++ controllers/youbot/youbot.c
@@ -215,29 +215,6 @@ int getIndexOfMin(float* array, size_t size){
   return minimum;
 }
 
-int getIndexOfMax(float* array, size_t size){
-  int maximum = 0;
-
-  for (int i = 1; i < size; i++)
-  {
-      if (array[i] > array[maximum])
-         maximum = i;
-  }
-  return maximum;
-}
-
-void bubble_sort(float array[], int size) {
-    for (int i = 0; i < (size-1); i++) {
-        for (int j = 0; j < (size-i-1); j++) {
-            if (array[j] > array[j+1]) {
-                float temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-            }
-        }
-    }
-}
-
 void print_array(float array[], int size) {
     for (int i = 0; i < size; i++) {
         printf("%f ", array[i]);
